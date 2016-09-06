e58d1cf07b4c83e8c806ea5b36345bc7507766ea
Jose Luis Martín <joseop21@gmail.com>
2016-09-07 01:05:33 +0200

    Completado el ejercicio fina del tema 2 con algunas modificaciones personales.

--- app/src/main/java/com/example/joselm/primerasvistas/MainActivity.java
+++ app/src/main/java/com/example/joselm/primerasvistas/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.joselm.primerasvistas;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,17 +11,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText entrada;
-    private TextView salida;
+    //private EditText entrada;
+    //private TextView salida;
+    private FragmentTabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        entrada = (EditText) findViewById(R.id.entrada);
-        salida = (TextView) findViewById(R.id.salida);
+        //entrada = (EditText) findViewById(R.id.entrada);
+        //salida = (TextView) findViewById(R.id.salida);
+        tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Primero"), Tab1.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Segundo"), Tab2.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Tercero"), Tab3.class, null);
     }
-
+    /*
     public void sePulsa(View view){
         salida.setText(String.valueOf(Float.parseFloat(entrada.getText().toString())*2.0));
     }
@@ -27,4 +35,5 @@ public class MainActivity extends AppCompatActivity {
     public void sePulsa0(View view){
         entrada.setText(entrada.getText() + "0");
     }
+    */
 }
---
+++ app/src/main/java/com/example/joselm/primerasvistas/Tab1.java
@@ -0,0 +1,29 @@
+package com.example.joselm.primerasvistas;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by joselm on 7/09/16.
+ */
+public class Tab1 extends Fragment{
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.tab1, container, false);
+    }
+}
